28be652242701ea08f0e2a0ff897d8aa158d9d21
falskuyyy <ahmdfalah10@gmail.com>
2023-05-25 21:15:44 +0700

    Membuat void doublelinkedlist::hapus

--- Doubleinkedlist_148/Doubleinkedlist_148.cpp
+++ Doubleinkedlist_148/Doubleinkedlist_148.cpp
@@ -118,3 +118,48 @@ void DoubleLinkedList::traverse() {
 	}
 }
 
+void DoubleLinkedList::traverse() {
+	if (listempty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecords is ascending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
+
+void DoubleLinkedList::revtraverse() {
+	if (listempty()) {
+		cout << "\nList is empty" << endl;
+	}
+
+	else {
+		cout << "\nRecords in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
+
+void DoubleLinkedList::hapus() {
+	if (listempty()) {
+		cout << "\nList is empty" << endl;
+
+	}
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	int rollNo;
+	cin >> rollNo;
+	cout << endl;
+	if (DoubleLinkedList::deleteNode(rollNo) == false)
+		cout << "Record not found" << endl;
+	else
+		cout << "Record with roll number " << " deleted" << endl;
+}
